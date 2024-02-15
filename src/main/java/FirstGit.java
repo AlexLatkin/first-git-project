cda155bbe2ccf2e290fb337cbff4162ff5480d78
Alexey <alexlatkin3@gmail.com>
2024-02-15 14:52:09 +0300

    Add sout in FirstGit

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -4,5 +4,6 @@ public class FirstGit {
         System.out.println("Git learning!!!");
         System.out.println("Sad");
         System.out.println("Happy");
+        System.out.println("!!!");
     }
 }
