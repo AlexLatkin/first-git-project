655e3102ba2a586e37579b4f6a34233debfc7fc5
Alexey <alexlatkin3@gmail.com>
2024-02-15 14:54:04 +0300

    Delete sout in FirstGit

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,7 +1,6 @@
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git develop");
-        System.out.println("Git learning!!!");
         System.out.println("Sad");
         System.out.println("Happy");
         System.out.println("!!!");
